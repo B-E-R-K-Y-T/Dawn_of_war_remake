ea88073c5faab8c463c407e171ad9f04c6fa3285
Tim <BerkyT115@yandex.ru>
2023-04-28 18:08:54 +0300

    created simple project

---
+++ Source/Dawn_of_war_remake/Dawn_of_war_remakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Dawn_of_war_remakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAWN_OF_WAR_REMAKE_API ADawn_of_war_remakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
